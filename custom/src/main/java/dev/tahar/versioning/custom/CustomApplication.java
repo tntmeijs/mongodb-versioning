87cfe2ea0d028696ef534f0ef58a0a991cd321d7
tntmeijs <tntmeijs@gmail.com>
2022-07-08 16:28:01 +0200

    chore: project set-up for custom versioning solution

---
+++ custom/src/main/java/dev/tahar/versioning/custom/CustomApplication.java
@@ -0,0 +1,13 @@
+package dev.tahar.versioning.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomApplication.class, args);
+	}
+
+}
---
+++ custom/src/test/java/dev/tahar/versioning/custom/CustomApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tahar.versioning.custom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
